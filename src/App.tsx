1394bd4ad8288d688492ffa1b21329104a887014
Antoinette Belebele <anthobelebele@gmail.com>
2024-08-29 10:07:17 +0100

    add style

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,75 @@
-import TaskList from './components/taskList';
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import { Task } from './components/types/task';
+import TaskItem from './components/taskItem';
+import { useLocalStorage } from './components/hooks/localstorage';
+import {  } from "./App.css";
 
+const generateId = () => `${Date.now()}-${Math.random().toString(36).substr(2, 9)}`;
 
+const TaskList: React.FC = () => {
+    const [tasks, setTasks] = useLocalStorage<Task[]>('tasks', []);
 
-const App: React.FC = () => {
-  return (
-      <div className="App">
-          <TaskList />
-      </div>
-  );
+    const [newTaskTitle, setNewTaskTitle] = useState('');
+
+    const addTask = () => {
+        if (!newTaskTitle.trim()) return;
+
+        const newTask: Task = {
+            id: generateId(),
+            title: newTaskTitle, 
+            completed: false,
+            createdAt: new Date(),
+        };
+        setTasks((prevTasks) => [...prevTasks, newTask]);
+        setNewTaskTitle('');
+    };
+
+    const toggleComplete = (id: string) => {
+        setTasks((prevTasks) =>
+            prevTasks.map((task) =>
+                task.id === id ? { ...task, completed: !task.completed } : task
+            )
+        );
+    };
+
+    const deleteTask = (id: string) => {
+        const confirmed = window.confirm('Vouz-vous supprimer la tâche ?');
+        if (confirmed) {
+            setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
+        }
+    };
+
+    const updateTask = (taskId: string, newName: string) => {
+        setTasks((prevTasks) =>
+            prevTasks.map((task) =>
+                task.id === taskId ? { ...task, title: newName } : task
+            )
+        );
+    };
+
+    return (
+        <div className="container">
+            <h1 className="title">Listes de tâches</h1>
+            <input
+                type="text"
+                value={newTaskTitle}
+                onChange={(e) => setNewTaskTitle(e.target.value)}
+                placeholder="Ajoutez une tâche..."
+            />
+            <button onClick={addTask}>Ajouter</button>
+            <div>
+                {tasks.map((task) => (
+                    <TaskItem
+                        key={task.id}
+                        task={task}
+                        toggleComplete={toggleComplete}
+                        deleteTask={deleteTask}
+                        onUpdate={updateTask}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 };
 
-export default App;
+export default TaskList;
